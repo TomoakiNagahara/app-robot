928dfca0d314abab2ada7432ac3cbce9a0b02ddd
Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
2017-03-21 04:05:35 +0900

    NEW: test/test.php

---
+++ test/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * app-robot:/test/test.php
+ *
+ * @creation  2017-03-21
+ * @version   1.0
+ * @package   app-robot
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+echo "Test was successful!\n";
